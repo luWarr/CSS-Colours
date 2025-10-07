b2df7c2590894635e6ba7a9f129b27a06abf2d4a
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-06 20:56:51 -0400

    test

---
+++ script.js
@@ -0,0 +1,3 @@
+//Used this youtube video made by: Bro Code
+//https://www.youtube.com/watch?v=37vxWr0WgQk
+// I used the originaly base code shown in the video, then modified it to cater to my API's specific needs
