52c24539d4c63bf54797c7d662e2d95975cb46c1
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-12 10:37:37 -0400

    Beginning fethc data (finally)

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //Used this youtube video made by: Bro Code
 //https://www.youtube.com/watch?v=37vxWr0WgQk
 // I used the originaly base code shown in the video, then modified it to cater to my API's specific needs
+
+fetch("https://csscolorsapi.com/api/colors")
+.then(response => console.log(response))
+.catch(error => console.error(error));
