ebc511e1e224bc438f86ea9b1af5a9910fd52182
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-12 19:47:17 +0100

    oh?

--- script.js
+++ script.js
@@ -18,8 +18,8 @@
 //Used this youtube video made by: Bro Code
 //https://www.youtube.com/watch?v=37vxWr0WgQk
 // I used the originaly base code shown in the video, then modified it to cater to my API's specific needs
-
-fetch("https://csscolorsapi.com/api/colors/red")
+//bre help as well
+fetch("https://corsproxy.io/?url=https://csscolorsapi.com/api/colors/red")
 .then(response => console.log(response))
 // .then(data => console.log(data))
 .catch(error => console.error(error));
