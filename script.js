aaa63e2ccf9821a46c314de52cb8f0aae4f67734
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-12 16:37:31 +0100

    testing B code

--- script.js
+++ script.js
@@ -19,134 +19,20 @@
 // .catch(error => console.error(error));
 
 //This code is from my friend Bre for trouble shooting
-// let url = new URL('https://csscolorsapi.com/api/colors');
-// url.searchParams.set('r', searchText);
-// url.searchParams.set('limit',10);
+let url = new URL('https://csscolorsapi.com/api/colors');
+url.searchParams.set('r', searchText);
+url.searchParams.set('limit',10);
 
-//THIS IS A TEST< IM NOT USING THIS
-const express = require("express");
-let colorRepo = require("./repos/colorRepo");
-const path = require("path");
-const cors = require("cors");
+console.log(url.href);
 
-const app = express();
-app.use(cors());
+//running console log
+fetch(url.href)
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+    displayResults(response.data);
+})
+.catch(er => console.error(er));
+//This is the end of the code i'm borrowing from my friend Bre
 
-// /colors/ gets all colors
-app.get("/api/colors", (req, res, next) => {
-	colorRepo.get(
-		function (data) {
-			res.status(200).json({
-				status: 200,
-				statusText: "OK",
-				message: "All css colors retrieved.",
-				count: data.length,
-				colors: data,
-			});
-		},
-		function (err) {
-			next(err);
-		}
-	);
-});
 
-// /colors/{name} gets a color by name
-app.get("/api/colors/:name", (req, res, next) => {
-	colorRepo.getByName(
-		req.params.name,
-		function (data) {
-			if (data) {
-				res.status(200).json({
-					status: 200,
-					statusText: "OK",
-					message: "Single color retrieved.",
-					data: data,
-				});
-			} else {
-				res.status(404).json({
-					status: 404,
-					statusText: "Not Found",
-					message: "The color could not be found.",
-					error: {
-						code: "NOT_FOUND",
-						message: `Color ${req.params.name} could not be found.`,
-					},
-				});
-			}
-		},
-		function (err) {
-			next(err);
-		}
-	);
-});
-
-// /colors/group/{name} gets all colors in a group
-app.get("/api/colors/group/:name", (req, res, next) => {
-	colorRepo.getByGroup(
-		req.params.name,
-		function (data) {
-			if (data.length > 0) {
-				res.status(200).json({
-					status: 200,
-					statusText: "OK",
-					message: `All ${req.params.name} colors retrieved.`,
-					count: data.length,
-					colors: data,
-				});
-			} else {
-				res.status(404).json({
-					status: 404,
-					statusText: "Not Found",
-					message: `Color group ${req.params.name} could not be found.`,
-					error: {
-						code: "NOT_FOUND",
-						message: `Color group ${req.params.name} could not be found.`,
-					},
-				});
-			}
-		},
-		function (err) {
-			next(err);
-		}
-	);
-});
-
-// /colors/theme/{name} gets all colors in a theme
-app.get("/api/colors/theme/:name", (req, res, next) => {
-	colorRepo.getByTheme(
-		req.params.name,
-		function (data) {
-			if (data.length > 0) {
-				res.status(200).json({
-					status: 200,
-					statusText: "OK",
-					message: `All ${req.params.name} theme colors retrieved.`,
-					count: data.length,
-					colors: data,
-				});
-			} else {
-				res.status(404).json({
-					status: 404,
-					statusText: "Not Found",
-					message: `Colors for theme ${req.params.name} could not be found.`,
-					error: {
-						code: "NOT_FOUND",
-						message: `Colors for theme ${req.params.name} could not be found.`,
-					},
-				});
-			}
-		},
-		function (err) {
-			next(err);
-		}
-	);
-});
-
-app.listen(4000, () => {
-	console.log("Running on port 4000.");
-});
-
-app.use(express.static(path.resolve("./public")));
-
-// Export the Express API
-module.exports = app;
