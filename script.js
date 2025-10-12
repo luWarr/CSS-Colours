6a8eebe7cd3172ea269b15928fe459f9ce511388
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-12 20:28:49 +0100

    does this work?

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-//I struggled to figure out CORS, I used code from this youtube video to troubleshoot
-//https://www.youtube.com/watch?v=PNtFSVU-YTI
-//Creator: Web Dev Simplified
-// const express = require('express')
-// const app = express()
-
-// app.get(fetch, (req, res) => {
-
-// })
-
-// app.listen
-
-
-
-
-
-
 //Used this youtube video made by: Bro Code
 //https://www.youtube.com/watch?v=37vxWr0WgQk
 // I used the originaly base code shown in the video, then modified it to cater to my API's specific needs
-//bre help as well
+
+//A friend gaves me a CORS bypass link so I can avoid messing around with it.
+//The link is: https://corsproxy.io/?url=
+
 fetch("https://corsproxy.io/?url=https://csscolorsapi.com/api/colors/red")
-.then(response => console.log(response))
-// .then(data => console.log(data))
+.then(response => response.json())
+.then(data => console.log(data))
 .catch(error => console.error(error));
 
 
