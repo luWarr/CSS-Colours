eb79f31d85f632c1e40999e304cf0f893080a9da
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-12 15:57:06 +0100

    testing once again

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 //https://www.youtube.com/watch?v=37vxWr0WgQk
 // I used the originaly base code shown in the video, then modified it to cater to my API's specific needs
 
-fetch("https://csscolorsapi.com/api/colors")
+fetch("https://csscolorsapi.com/api/colors/red")
 .then(response => console.log(response))
 .catch(error => console.error(error));
+
+//This code is from my friend Bre for trouble shooting
+// let url = new URL('https://csscolorsapi.com/api/colors');
+// url.searchParams.set('r', searchText);
+// url.searchParams.set('limit',10);
+
