1fe7f0f1e58b79474e8a26c6f976d3693035c1bb
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-12 16:20:59 +0100

    troubleshooting

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+//This code is from Co-pilot for trouble shooting. Whenever I try to fetch the API data, in the console log it says error 404. A friend suggested adding CORS to the code.
+// ...existing code...
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); // This enables CORS for all routes
+
+// ...your routes and other code...
+
+
 //Used this youtube video made by: Bro Code
 //https://www.youtube.com/watch?v=37vxWr0WgQk
 // I used the originaly base code shown in the video, then modified it to cater to my API's specific needs
 
 fetch("https://csscolorsapi.com/api/colors/red")
-// .then(response => response.json())
+.then(response => response.json())
 .then(data => console.log(data))
-// .catch(error => console.error(error));
+.catch(error => console.error(error));
 
 //This code is from my friend Bre for trouble shooting
 // let url = new URL('https://csscolorsapi.com/api/colors');
