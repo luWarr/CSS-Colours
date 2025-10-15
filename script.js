24aa6fe9c6611847b50e1b18d754c71ea43b25da
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-15 19:26:26 -0400

    o

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function fetchData(){
         }
 
         const data = await response.json();
-        const color = data.data; // There is nested data within my API, Co-pilot suggested this line of code to assit with getting the proper data out.
+        const colors = data.data; // There is nested data within my API, Co-pilot suggested this line of code to assit with getting the proper data out.
         //This code came from Co-Pilot
         //Prompt: "what code should I place in the html file in order to display my data from this api https://csscolorsapi.com/api/colors/{color name}?"
        
