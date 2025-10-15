5629f5bf3a494f0f3c38cd32dfb5678cd758bca6
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-14 21:37:18 -0400

    trying something

--- script.js
+++ script.js
@@ -6,81 +6,96 @@
 //The link is: https://corsproxy.io/?url=
 
 fetch("https://corsproxy.io/?url=https://csscolorsapi.com/api/colors")
-.then(response => response.json())
-.then(data => console.log(data))
+.then(response => console.log(response))
+// .then(response => response.json())
+// .then(data => console.log(data))
 .catch(error => console.error(error));
 
 
-// This javascript code is from Harold's IMDB project from GitHub, with my idea using this code as a base to build on was super important and very helpful
-// Link:https://github.com/ixd-system-design/UI-for-Data-Fetching/tree/main/movies
-// My goal for using this javascript from harold was to get a better understand about it in general. I don't know how to code very well so doing a basic setup was within the scope of my project.
-
-const mainContent = document.querySelector('#mainContent')
-const searchBubble = document.querySelector('#searchBubble')
-
-// This is to get stuff out of the search bubble from the HTML file. 
-let getText = document.querySelector('#getText').value
-
-// Get all the colours from the API and filter them through these parameters from the data of the API
-//I want the user to look up the general group of the colour they are looking for in the text box.
-let url = new URL('https://corsproxy.io/?url=https://csscolorsapi.com/api/colors');
-url.searchParams.set('group',getText)
-url.searchParams.set("limit", 20)
+ 
 
 
-console.log(url.href)
-
-// In this one we're running the fetch request fro the data, then we can watch it in the console log to make it sure it works.
-fetch(url.href, options)
-.then(response => response.json())
-.then(response => {
-    console.log(response)
-    displayResults(response.results)
-})
-.catch(err => console.error(err));
 
 
-const displayResults = (results) => {
-    mainContent.innerHTML = '';
-    if (results) {
-        results.forEach(result => {
-            let div = document.createElement('div');
-            div.classList.add('result');
 
-            div.innerHTML = `
-                <div>
-                    <p class="group">${result.group}</p>
-                    <p class="name">${result.name}</p>
-                    <p class="hex">${result.hex}</p>
-                    <p class="rgb">${result.rgb}</p>
-                </div>
-            `;
-            mainContent.appendChild(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // This javascript code is from Harold's IMDB project from GitHub, with my idea using this code as a base to build on was super important and very helpful
+// // Link:https://github.com/ixd-system-design/UI-for-Data-Fetching/tree/main/movies
+// // My goal for using this javascript from harold was to get a better understand about it in general. I don't know how to code very well so doing a basic setup was within the scope of my project.
+
+// const mainContent = document.querySelector('#mainContent')
+// const searchBubble = document.querySelector('#searchBubble')
+
+// // This is to get stuff out of the search bubble from the HTML file. 
+// let getText = document.querySelector('#getText').value
+
+// // Get all the colours from the API and filter them through these parameters from the data of the API
+// //I want the user to look up the general group of the colour they are looking for in the text box.
+// let url = new URL('https://corsproxy.io/?url=https://csscolorsapi.com/api/colors');
+// url.searchParams.set('group',getText)
+// url.searchParams.set("limit", 20)
+
+
+// console.log(url.href)
+
+// // In this one we're running the fetch request fro the data, then we can watch it in the console log to make it sure it works.
+// fetch(url.href, options)
+// .then(response => response.json())
+// .then(response => {
+//     console.log(response)
+//     displayResults(response.results)
+// })
+// .catch(err => console.error(err));
+
+
+// const displayResults = (results) => {
+//     mainContent.innerHTML = '';
+//     if (results) {
+//         results.forEach(result => {
+//             let div = document.createElement('div');
+//             div.classList.add('result');
+
+//             div.innerHTML = `
+//                 <div>
+//                     <p class="group">${result.group}</p>
+//                     <p class="name">${result.name}</p>
+//                     <p class="hex">${result.hex}</p>
+//                     <p class="rgb">${result.rgb}</p>
+//                 </div>
+//             `;
+//             mainContent.appendChild(div);
             
         
-        })
-    }
-    else {
-        mainContent.innerHTML = '<p>Sorry! Nothing here, try again.</p>';
-    }
-};
-
-
-
-
-///chat GPT code below///
-//This function is for the red button on the main page. This was just a reminder so I knew what to do.
-// function goToRed() {
-//   window.location.href = 'red.html';
-// }
-
-// function goToBrown() {
-//   window.location.href = 'brown.html';
-// }
-
-// function goToOrange() {
-//   window.location.href = 'orange.html';
-// }
-
+//         })
+//     }
+//     else {
+//         mainContent.innerHTML = '<p>Sorry! Nothing here, try again.</p>';
+//     }
+// };
 
 
