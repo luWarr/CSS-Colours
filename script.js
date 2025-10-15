f26c56690c8a6465a4f8cac5a498d107f4c09e19
Lucy Warren <warreluc@sheridancollege.ca>
2025-10-15 19:24:06 -0400

    copilot suggestion

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function fetchData(){
     try{
         
         const searchValue = document.getElementById("searchValue").value.toLowerCase();
-        const response = await fetch(`https://corsproxy.io/?url=https://csscolorsapi.com/api/colors/group/${searchValue}`);
+        const response = await fetch(`https://corsproxy.io/?url=https://csscolorsapi.com/api/colors?group=${searchValue}`);
 
         if(!response.ok){
             throw new Error("could not get data");
@@ -23,12 +23,16 @@ async function fetchData(){
        
         const contentArea = document.getElementById("contentarea");
         
-          if (color && color.name && color.hex && color.rgb) {
-        contentArea.innerHTML = `
-            <h2>${color.name}</h2>
+          // if (color && color.name && color.hex && color.rgb) <----This is to fetch a single color
+          // The line below should fetch more than one (in theory)
+                if (Array.isArray(colors) && colors.length > 0)  {
+        contentArea.innerHTML = colors.map(color =>
+          `<div style="margin-bottom:20px; padding:10px; border:1px solid #eee; border-radius:8px;">
+          <h2>${color.name}</h2>
             <p>Hex: ${color.hex}</p>
             <p>RGB: ${color.rgb}</p>
-            <div style="width:50px; height:50px; background:${color.hex}; border-radius:8px; border:1px"></div>`;
+            <div style="width:50px; height:50px; background:${color.hex}; border-radius:8px; border:1px"></div>`
+        ).join('');
           } else {
         contentArea.innerHTML = '<p>Sorry! Nothing here, try again.</p>';
           }
@@ -71,62 +75,5 @@ async function fetchData(){
 
 
 
-
-
-
-
-// // This javascript code is from Harold's IMDB project from GitHub, with my idea using this code as a base to build on was super important and very helpful
-// // Link:https://github.com/ixd-system-design/UI-for-Data-Fetching/tree/main/movies
-// // My goal for using this javascript from harold was to get a better understand about it in general. I don't know how to code very well so doing a basic setup was within the scope of my project.
-
-// const mainContent = document.querySelector('#mainContent')
-// const searchBubble = document.querySelector('#searchBubble')
-
-// // This is to get stuff out of the search bubble from the HTML file. 
-// let getText = document.querySelector('#getText').value
-
-// // Get all the colours from the API and filter them through these parameters from the data of the API
-// //I want the user to look up the general group of the colour they are looking for in the text box.
-// let url = new URL('https://corsproxy.io/?url=https://csscolorsapi.com/api/colors');
-// url.searchParams.set('group',getText)
-// url.searchParams.set("limit", 20)
-
-
-// console.log(url.href)
-
-// // In this one we're running the fetch request fro the data, then we can watch it in the console log to make it sure it works.
-// fetch(url.href, options)
-// .then(response => response.json())
-// .then(response => {
-//     console.log(response)
-//     displayResults(response.results)
-// })
-// .catch(err => console.error(err));
-
-
-// const displayResults = (results) => {
-//     mainContent.innerHTML = '';
-//     if (results) {
-//         results.forEach(result => {
-//             let div = document.createElement('div');
-//             div.classList.add('result');
-
-//             div.innerHTML = `
-//                 <div>
-//                     <p class="group">${result.group}</p>
-//                     <p class="name">${result.name}</p>
-//                     <p class="hex">${result.hex}</p>
-//                     <p class="rgb">${result.rgb}</p>
-//                 </div>
-//             `;
-//             mainContent.appendChild(div);
-            
-        
-//         })
-//     }
-//     else {
-//         mainContent.innerHTML = '<p>Sorry! Nothing here, try again.</p>';
-//     }
-// };
 
 
